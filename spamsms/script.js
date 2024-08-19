4821b80715ae77ae5fb644fc57bfc9e995c3be1c
huynhquangdev <141423816+Quangqq@users.noreply.github.com>
2024-08-19 09:55:15 +0700

    Update script.js

--- spamsms/script.js
+++ spamsms/script.js
@@ -1,5 +1,5 @@
 document.getElementById('smsForm').addEventListener('submit', function(event) {
-    event.preventDefault(); // Prevent the form from submitting the traditional way
+    event.preventDefault(); // Prevent form from submitting the traditional way
 
     const phone = document.getElementById('phone').value;
     const amout = document.getElementById('amout').value;
@@ -7,17 +7,19 @@ document.getElementById('smsForm').addEventListener('submit', function(event) {
     const apiUrl = 'https://api.nqtool.net/spamsms/';
     const params = new URLSearchParams({ phone: phone, amout: amout });
 
-    fetch(`${apiUrl}?${params}`)
-        .then(response => response.json())
-        .then(data => {
-            if (data.success) {  // Check if the API response indicates success
-                alert('Request successful!');
-            } else {
-                alert('Request failed. Please try again.');
-            }
-        })
-        .catch(error => {
-            console.error('Error:', error);
-            alert('An error occurred. Please try again later.');
-        });
+    fetch(`${apiUrl}?${params}`, {
+        method: 'GET',
+    })
+    .then(response => response.json())
+    .then(data => {
+        if (data.success) {
+            alert('Request successful!');
+        } else {
+            alert('Request failed. Please try again.');
+        }
+    })
+    .catch(error => {
+        console.error('Error:', error);
+        alert('An error occurred. Please try again later.');
+    });
 });
