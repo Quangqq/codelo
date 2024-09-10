fe39f4ebef60c8b761967a3ad17d167b3d4cb20d
huynhquangdev <141423816+Quangqq@users.noreply.github.com>
2024-09-10 20:37:10 +0700

    Update script.js

--- t/mp/script.js
+++ t/mp/script.js
@@ -1,7 +1,15 @@
 document.getElementById('fetchVideoBtn').addEventListener('click', function() {
     fetch('https://api.sumiproject.net/video/videoanime')
-        .then(response => response.json())
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Network response was not ok ' + response.statusText);
+            }
+            return response.json();
+        })
         .then(data => {
+            if (!data.url || !data.count) {
+                throw new Error('Invalid data format');
+            }
             const videoContainer = document.getElementById('video-container');
             videoContainer.innerHTML = `
                 <video controls>
@@ -11,5 +19,9 @@ document.getElementById('fetchVideoBtn').addEventListener('click', function() {
                 <div id="video-info">View Count: ${data.count}</div>
             `;
         })
-        .catch(error => console.error('Error fetching the video:', error));
+        .catch(error => {
+            console.error('Error fetching the video:', error);
+            const videoContainer = document.getElementById('video-container');
+            videoContainer.innerHTML = `<p>Error fetching video: ${error.message}</p>`;
+        });
 });
